9aff17ff2ce0bbf9e0b6a36026d802dd12943ef7
Shiva Krishna Gopannagari <34725150+shivakrishna1995@users.noreply.github.com>
2026-06-24 23:24:35 +0530

    Add: is-palindrome - submission-4

---
+++ Data Structures & Algorithms/is-palindrome/submission-4.js
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+        let l = 0, r = s.length - 1;
+
+        while (l < r) {
+            while (!this.alphaNum(s[l]) && l < r) l++;
+            while (!this.alphaNum(s[r]) && l < r) r--;
+
+            if (s[l].toLowerCase() !== s[r].toLowerCase()) return false;
+            
+            l++;
+            r--;
+        }
+
+        return true;
+    }
+
+    alphaNum(c) {
+        return (
+            (c >= 'A' && c <= 'Z') ||
+            (c >= 'a' && c <= 'z') ||
+            (c >= '0' && c <= '9')
+        )
+    }
+}
